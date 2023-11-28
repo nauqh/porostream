5042f54993b17a06673516d20a0692e62dc9d902
Nauqh <hodominhquan.work@gmail.com>
2023-11-28 21:58:32 +0700

    edit title font size

--- app/utils/graph.py
+++ app/utils/graph.py
@@ -240,8 +240,12 @@ def graph_dmgpersonal(matchdf, playerdf):
         marker=dict(color="#ffc300")
     ))
 
-    fig.update_layout(title="Gold by Damage and CS", height=600,
-                      legend=dict(orientation="h", yanchor="top", xanchor="center", x=0.5, y=1.1))
+    fig.update_layout(title={
+        'text': "Gold by Damage and CS",
+        "font_size": 15
+    },
+        height=600,
+        legend=dict(orientation="h", yanchor="top", xanchor="center", x=0.5, y=1.1))
 
     fig.update_yaxes(title=None, showgrid=False)
     fig.update_yaxes(secondary_y=True, range=[0, 10])
