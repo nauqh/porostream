b3d3c737e4093f043dd6b32aae0ceb1745ba9d86
Nauqh <hodominhquan.work@gmail.com>
2023-11-29 13:30:59 +0700

    edit

--- app/main.py
+++ app/main.py
@@ -119,7 +119,8 @@ data = team[selected_player]
 l, m, r = st.columns([1, 1, 1])
 
 with l:
-    st.image(f"img/rank/{data['tier']}.png")
+    # st.image(f"img/rank/{data['tier']}.png")
+    st.image(f"img/poros.png")
 
 with m:
     queue = {'RANKED_SOLO_5x5': 'Soloqueue',
