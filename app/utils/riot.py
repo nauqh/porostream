9373cb7ffd2b72383258537aaa303fcd98846e99
Nauqh <hodominhquan.work@gmail.com>
2023-11-30 15:51:53 +0700

    edit

--- app/utils/riot.py
+++ app/utils/riot.py
@@ -97,8 +97,8 @@ def transform(match_df: pd.DataFrame, player_df: pd.DataFrame):
     stats.update({
         'dmg': player_df['totalDamageDealtToChampions'].mean(),
         'penta': player_df['pentaKills'].sum(),
-        'wins': player_df['win'].value_counts().values[1],
-        'loses': player_df['win'].value_counts().values[0],
+        'wins': player_df['win'].value_counts().to_dict()[True],
+        'loses': player_df['win'].value_counts().to_dict()[False],
     })
 
     # Achievements (time in sec)
