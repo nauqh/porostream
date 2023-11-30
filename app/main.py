3b9a282afc72033ef7352392dd96c1a0ce497290
Nauqh <hodominhquan.work@gmail.com>
2023-11-30 15:38:14 +0700

    edit

--- app/main.py
+++ app/main.py
@@ -274,7 +274,7 @@ if run:
 
         # NOTE: STATS
         st.write("##")
-        st.header("☕Champions")
+        st.header("Champions")
         stats = ['totalDamageDealtToChampions', 'kills', 'deaths', 'assists']
 
         # Create a DataFrame with the aggregated data
